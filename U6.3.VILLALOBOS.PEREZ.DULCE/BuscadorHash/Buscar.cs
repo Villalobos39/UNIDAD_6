b976a4d459704df081f4f430ea5536110dbd1001
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2018-12-07 17:53:37 -0800

    B. HASH

---
+++ U6.3.VILLALOBOS.PEREZ.DULCE/BuscadorHash/Buscar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscadorHash
+{
+    class Buscar
+    {
+        string opc;
+        Hashtable Buscador = new Hashtable();
+        bool encontrado = false;
+        public void BuscadorH()
+        {
+            //ESTE METODO DE BUSQUEDA TIENE COMO PRINCIPAL FUNCION EL USO DE 
+            //PALABRAS O CARACTERES CLAVES, DONDE PUEDEN IDENTIFICAR 
+            Console.Write("\nBUSCAR DE LIBROS ISBN \n");
+            //USAMOS LA COLLECTION POR DEFECTO DEL C# Hasttable
+           Console.Write("\nISBN    ||   LIBROS  \n\n");
+            Buscador.Add("5545588", "ARISTOTELES Y DANTE ");
+            Buscador.Add("3554666", "LAS VENTAJAS DE SER INVISIBLE");
+            Buscador.Add("2258967", "LA CHICA DEL TREEN ");
+            Buscador.Add("2145698", "BUSCANDO ALASKA ");
+            Buscador.Add("6548828", "LA LADRONA DEL LIBRO  ");
+            Buscador.Add("7892463", "FURA DE MI ");
+            Buscador.Add("3654882", "EL CODIGO DA VINCI");
+            Buscador.Add("4587621", "DRACULA ");
+            Imprimir(Buscador); //AÑADIMOS LOS CAMPOS 
+            do
+            {
+                Console.Write("\nBUSCAR ISBN: ");
+                opc = Console.ReadLine();
+                //SE LE PIDE AL USUARIO QUE INGRESE SU BUSQUEDA EN ESTE CASO ES POR EL CODIGO UNICO DE LIBROS
+                //ESTO DETERMINA SI SE ENCUENTRA O NO 
+                if (Buscador.ContainsKey(opc))
+                { //SI LA CARACTERISTICA CLAVE EXISTE EN EL CONTESTO DE LA TABLA HASH
+                    Console.WriteLine("\nEL LIBRO : " + Buscador[opc] +" || SI SE ENCUENTRA ");
+                    encontrado = true; // SI EL ELEMENTO FUE ENCONTRADO EL CICLO SE DETIENE 
+                }
+                else Console.WriteLine("\nNO SE EN CONTRO");
+            }
+            while (encontrado == false); // DE LO CONTRARIO SE REPITE HATSA QUE INGLESE UNO QUE SE ENCUENTRE 
+        }
+
+        public static void Imprimir(Hashtable Tabla)
+        { //ESTE METODO IMPRIMI LOS DATOS AÑADISOS TAL COMO SON IGRESADOS 
+            foreach (DictionaryEntry elemento in Tabla)
+            { //Y ASI TAMBIEN EL ELEMNTO DE CADA CLAVE 
+
+                Console.WriteLine("{0} || {1}", elemento.Key, elemento.Value);
+            }
+        }
+    }
+}
---
+++ U6.3.VILLALOBOS.PEREZ.DULCE/BuscadorHash/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace BuscadorHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //El método llamado por transformación de claves(hash), permite aumentar la
+            //velocidad de búsqueda sin necesidad de tener los elementos ordenados.Cuenta
+            //también con la ventaja de que el tiempo de búsqueda es prácticamente
+            //independiente del número de componentes del arreglo.
+            // Trabaja basándose en una función de transformación o función hash(H) que
+            //convierte una clave en una dirección(índice) dentro del arreglo.
+
+            Buscar buscar = new Buscar();
+            buscar.BuscadorH();
+            Console.ReadKey();
+        }
+    }
+
+}
